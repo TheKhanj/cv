ca12c89081e29aa4a1c808affdf3f327a9268a4e
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-08-14 16:21:20 +0330

    add build scripts

---
+++ scripts/create_html.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+input="$1"
+output="$2"
+tempalte="$3"
+
+markdown -S "$input" >tmp.html
+
+while read -r line; do
+	if [ "$line" = '{{main-content}}' ]; then
+		cat tmp.html
+	else
+		echo "$line"
+	fi
+done <"$tempalte" >"$output"
+
+rm tmp.html
